21e48d9095c8859d558f12c9526d5d72dd76c15d
Gia Hai <haidao549@gmail.com>
2024-12-19 08:23:03 +0700

    update cached query

--- app/api/routes/chat_bot.py
+++ app/api/routes/chat_bot.py
@@ -1,6 +1,12 @@
+import hashlib
+import struct
+
+import numpy as np
 from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from pydantic import BaseModel, SecretStr
 from redis import Redis
+from scipy.spatial.distance import cosine
 
 from app.core.config import settings
 from app.services.rag_service import RAGServices
@@ -15,15 +21,56 @@ class QuestionRequest(BaseModel):
 redis = Redis(host=settings.REDIS_ENDPOINT, port=settings.REDIS_PORT, password=settings.REDIS_PASSWORD,
               decode_responses=True)
 
+model = GoogleGenerativeAIEmbeddings(
+    model="models/text-embedding-004", google_api_key=SecretStr(settings.GOOGLE_API_KEY))
+
+
+def normalize_query(query: str) -> str:
+    import re
+    query = query.lower().strip()
+    query = re.sub(r"[^a-z0-9\s]", "", query)
+    return query
+
+
+def get_cache_key(query: str) -> str:
+    normalized_query = normalize_query(query)
+    embedding = model.embed_query(normalized_query)
+    embedding_bytes = struct.pack(f"{len(embedding)}f", *embedding)
+    return hashlib.md5(embedding_bytes).hexdigest()
+
+
+async def get_similar_cached(query: str):
+    normalized_query = normalize_query(query)
+    embedding = model.embed_query(normalized_query)
+    embedding_bytes = struct.pack(f"{len(embedding)}f", *embedding)
+
+    for key in await redis.keys("*"):
+        cached_embedding = await redis.get(key)
+        cached_embedding = np.frombuffer(cached_embedding, dtype=np.float32)
+        similarity = 1 - cosine(embedding_bytes, cached_embedding)
+        if similarity > 0.9:
+            return await redis.get(key)
+    return None
+
 
 @router.post("/send_message", response_model=dict)
 async def read_root(body: QuestionRequest):
     try:
         user_question = body.user_question
+        cached_key = get_similar_cached(user_question)
+
+        if cached_key:
+            return {
+                "cached": True,
+                "response": cached_key
+            }
+
         rag_services = RAGServices(data=None)
         response, retrieved_contexts, num_contexts = rag_services.get_rag(
             user_question)
+
         return {
+            "cached": False,
             "response": response,
             "num_contexts": num_contexts,
             "retrieved_contexts": retrieved_contexts
