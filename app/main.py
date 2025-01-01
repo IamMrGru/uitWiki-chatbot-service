f298af38b96f5c6abc863204ecec936b01d605b4
haidg <haidg@vng.com.vn>
2025-01-01 12:53:32 +0700

    change health check

--- app/main.py
+++ app/main.py
@@ -1,39 +1,13 @@
-import gc
-from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from mangum import Mangum
 
 from app.api import main as api
 from app.core.config import settings
-from app.core.database import MongoDBConnection
-
-mongodb = MongoDBConnection()
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    try:
-        await mongodb.connect()
-        yield
-    finally:
-        await mongodb.close()
-        gc.collect()
-
-
-async def get_db():
-    return mongodb.db
-
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    lifespan=lifespan
 )
-
-handler = Mangum(app)
-
 
 origins = [
     "http://localhost:3000",
@@ -50,7 +24,7 @@ app.add_middleware(
 
 @app.get("/", tags=["Root"])
 def read_root():
-    return {"message": "Hello World"}
+    return True
 
 
 app.include_router(api.api_router, prefix=settings.API_V1_STR)
